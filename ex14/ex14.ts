dfe0ba2d500e3d4ebd296bafadfb944abc92038c
khanhtran223 <khanhtnk234111e@st.uel.edu.vn>
2026-01-19 00:29:35 +0700

    Add files via upload

---
+++ ex14/ex14.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ex14',
+  standalone: false,
+  templateUrl: './ex14.html',
+  styleUrl: './ex14.css',
+})
+export class Ex14 {
+
+}
